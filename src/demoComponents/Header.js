01d8fd2348c05930976b69a79683cd8c957b0035
hoang thanh nghia <thanhnghia1712@gmail.com>
2022-12-29 16:34:48 +0700

    first commit

---
+++ src/demoComponents/Header.js
@@ -0,0 +1,17 @@
+import {Component} from "react";
+// import css
+import "./Header.css";
+// class component
+class Header extends Component{
+    render(){
+        
+        return (
+            <div className="container">
+                <h1 className="title">Header</h1>
+            <span>This is a header</span>
+            </div>
+        );
+    }
+}
+
+export default Header;
